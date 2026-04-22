6014abc6413265f87f8e12732ec80b3002a0f9ca
Sifti Al Mahmud <siftiam@gmail.com>
2026-04-23 02:35:45 +0600

    Initializes the Project

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
